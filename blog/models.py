06ca6d8d9038408e845ea6def46b4c24fea0847d
miguelms19 <miguelms19@gmail.com>
2018-03-20 19:58:52 +0000

    added bootstrap for jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class BlogConfig(models.Model):
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+#Add Blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# add to the admin
