f28660562723efb566607cc01929afbd616e9b31
miguelms19 <miguelms19@gmail.com>
2018-03-22 17:02:16 +0000

    added jobs to home

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-#from .models import Blog
+from .models import JobsConfig
 
-#admin.site.register(Blog)
+admin.site.register(JobsConfig)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import JobsConfig
 # Create your views here.
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = JobsConfig.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
